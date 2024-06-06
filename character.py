e42e3d7d45c80dc7012cde9ab15026d1e5a2d7ac
fOuttaMyPaint <154358121+fOuttaMyPaint@users.noreply.github.com>
2024-06-06 04:44:59 -0400

    Update character.py

--- character.py
+++ character.py
@@ -5,17 +5,10 @@ class Character:
         self.attack = attack
         self.defense = defense
 
-    def is_alive(self):
-        return self.health > 0
+    def is_dead(self):
+        return self.health <= 0
 
     def take_damage(self, damage):
         self.health -= damage
         if self.health < 0:
             self.health = 0
-
-    def attack_enemy(self, enemy):
-        damage_dealt = self.attack - enemy.defense
-        if damage_dealt < 0:
-            damage_dealt = 0
-        enemy.take_damage(damage_dealt)
-        return damage_dealt
