4f0e5c667604f580835a077a908db559e6ada166
fOuttaMyPaint <154358121+fOuttaMyPaint@users.noreply.github.com>
2024-06-06 04:13:23 -0400

    Update item.py

--- item.py
+++ item.py
@@ -1,7 +1,11 @@
 class Item:
-    def __init__(self, name, description):
+    def __init__(self, name, description, item_type, health_bonus=0, attack_bonus=0, defense_bonus=0):
         self.name = name
         self.description = description
+        self.type = item_type
+        self.health_bonus = health_bonus
+        self.attack_bonus = attack_bonus
+        self.defense_bonus = defense_bonus
 
     def __str__(self):
         return self.name
