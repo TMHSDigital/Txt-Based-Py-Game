d6a8d5210ee9117016537f6a538f624826efc4ac
fOuttaMyPaint <154358121+fOuttaMyPaint@users.noreply.github.com>
2024-06-06 04:13:43 -0400

    Create enemy.py

---
+++ enemy.py
@@ -0,0 +1,7 @@
+class Enemy:
+    def __init__(self, name, health, attack, defense, drops):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.drops = drops
